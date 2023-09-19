2e97b6abca5616ad71acd90b4a425e13727a322d
Ashton Cross <90781919+AshtonCross@users.noreply.github.com>
2023-09-19 04:57:25 -0600

    emacs config revamp
    auto generated from cool site. new packages. experimentation will be done

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,84 +1,171 @@
-;;
-;; package managers
-;;
+;;; package --- Summary
 
+;;; Commentary:
+;;; generated  using https://emacs.amodernist.com/ with tweaks from my
+;;; old config.
+
+;;; Personal configuration -*- lexical-binding: t -*-
+
+;; Save the contents of this file under ~/.emacs.d/init.el
+;; Do not forget to use Emacs' built-in help system:
+;; Use C-h C-h to get an overview of all help commands.  All you
+;; need to know about Emacs (what commands exist, what functions do,
+;; what variables specify), the help system can provide.
+
+;; Add the NonGNU ELPA package archive
 (require 'package)
-
+(add-to-list 'package-archives  '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
+(unless package-archive-contents  (package-refresh-contents))
 
-(package-initialize)
-(package-refresh-contents)
+;; Load a custom theme
+(load-theme 'deeper-blue t)
 
-;; Use package
+;; Disable the menu bar
+(menu-bar-mode -1)
 
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(require 'use-package)
+;; Disable the tool bar
+(tool-bar-mode -1)
 
-;; general settings
+;; Disable splash screen
+(setq inhibit-startup-screen t)
 
-;; dired conf
-(put 'dired-find-alternate-file 'disabled nil)
+;; Enable line numbering in `prog-mode'
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
 
-;; custom.el 
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
+;;; LSP Support
+(unless (package-installed-p 'eglot)
+  (package-install 'eglot))
 
-;; org mode conf
-(add-hook 'org-mode-hook 'org-indent-mode)
+;; Enable LSP support by default in programming buffers
+(add-hook 'prog-mode-hook #'eglot-ensure)
 
-;; display line numbers 
-(when (version<= "26.0.50" emacs-version )
-;;  (setq display-line-numbers-type 'relative)
-  (global-display-line-numbers-mode))
+;;; Inline static analysis
 
-;; tweaks to ui
+;; Enabled inline static analysis
+(add-hook 'prog-mode-hook #'flymake-mode)
+
+;; Display messages when idle, without prompting
+(setq help-at-pt-display-when-idle t)
+
+;;; Pop-up completion
+(unless (package-installed-p 'corfu)
+  (package-install 'corfu))
+
+;; Enable autocompletion by default in programming buffers
+(add-hook 'prog-mode-hook #'corfu-mode)
+
+;;; Git client
+(unless (package-installed-p 'magit)
+  (package-install 'magit))
+
+;; Bind the `magit-status' command to a convenient key.
+(global-set-key (kbd "C-c g") #'magit-status)
+
+;;; Indication of local VCS changes
+(unless (package-installed-p 'diff-hl)
+  (package-install 'diff-hl))
+
+;; Enable `diff-hl' support by default in programming buffers
+(add-hook 'prog-mode-hook #'diff-hl-mode)
+
+;;; Clojure Support
+(unless (package-installed-p 'clojure-mode)
+  (package-install 'clojure-mode))
+
+;;; Haskell Support
+(unless (package-installed-p 'haskell-mode)
+  (package-install 'haskell-mode))
+
+;;; Lua Support
+(unless (package-installed-p 'lua-mode)
+  (package-install 'lua-mode))
+
+;;; Additional Lisp support
+(unless (package-installed-p 'sly)
+  (package-install 'sly))
+
+;;; LaTeX support
+(unless (package-installed-p 'auctex)
+  (package-install 'auctex))
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+;; Enable LaTeX math support
+(add-hook 'LaTeX-mode-map #'LaTeX-math-mode)
+
+;; Enable reference mangment
+(add-hook 'LaTeX-mode-map #'reftex-mode)
+
+;;; Markdown support
+(unless (package-installed-p 'markdown-mode)
+  (package-install 'markdown-mode))
+
+;;; Outline-based notes management and organizer
+
+;;; Additional Org-mode related functionality
+(unless (package-installed-p 'org-contrib)
+  (package-install 'org-contrib))
+
+;;; IRC Client
+
+;; Connect to Librea
+(setq rcirc-server-alist
+      '(("irc.libera.chat" :channels ("#emacs")
+         :port 6697 :encryption tls)))
+
+;; Set your IRC nick
+(setq rcirc-default-nick "nil")
+(add-hook 'rcirc-mode-hook #'rcirc-track-minor-mode)
+(add-hook 'rcirc-mode-hook #'rcirc-omit-mode)
+
+;; Miscellaneous options
+(setq-default major-mode
+              (lambda () ; guess major mode from file name
+                (unless buffer-file-name
+                  (let ((buffer-file-name (buffer-name)))
+                    (set-auto-mode)))))
+(setq confirm-kill-emacs #'yes-or-no-p)
+(setq window-resize-pixelwise t)
+(setq frame-resize-pixelwise t)
+(save-place-mode t)
+(savehist-mode t)
+(recentf-mode t)
+(defalias 'yes-or-no #'y-or-n-p)
+
+;; Store automatic customisation options elsewhere
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+
+(load (expand-file-name "~/.quicklisp/slime-helper.el"))
+  ;; Replace "sbcl" with the path to your implementation
+  (setq inferior-lisp-program "sbcl")
+
+;;
+;; legacy config stuff
+;;
 
 (display-battery-mode 1)
 (display-time-mode 1)
-(tool-bar-mode -1)
 (toggle-scroll-bar -1)
 (blink-cursor-mode 0)
-
-;; other tweaks
 (setq scroll-conservatively 100)
 (global-prettify-symbols-mode 1)
-
-;; gruvbox
-
-(use-package gruvbox-theme
-  :ensure t)
-(load-theme 'gruvbox-dark-hard)
-;;(load-theme 'gruvbox-light-hard)
-
-(use-package almost-mono-themes
-  :ensure t)
-(load-theme 'almost-mono-gray)
-
-;; for some reason, loading both of these themes creats a really cool
-;; theme
 
 ;; curosr
 (setq-default cursor-type 'box)
 (set-cursor-color "#fff000")
 (global-hl-line-mode t)
 
-;;
 ;; elfeed
-;;
 
-(use-package elfeed
-  :ensure t)
-
-(global-set-key (kbd "C-x w") 'elfeed) ; nice bind
-
-;; G - Fetch feed updates
-;; g - refresh view of feed listing
-;; s - update serach filter
-;; c - clear search filter
+(unless (package-installed-p 'elfeed)
+  (package-install 'elfeed))
 
 (setq elfeed-feeds
       '(
@@ -91,9 +178,8 @@
 	("https://notrelated.xyz/rss" philosophy podcast)
 	))
 
-;;
-;; behavior
-;;
+;; org mode conf
+(add-hook 'org-mode-hook 'org-indent-mode)
 
 ;; move those annoying ***~ files to a nice spot
 (setq make-backup-files nil)
@@ -122,20 +208,6 @@
 (setf use-default-font-for-symbols nil)
 (set-fontset-font t 'unicode "Noto Emoji" nil 'append)
 
-;; auctex
-;;(use-package auctex
-;;  :ensure t)
-
-;;;
-;;; bindings
-;;;
-
-;;(use-package evil :ensure t) ; vi keybindings
-;;(evil-mode)
-
-;; (use-package ergoemacs-mode :ensure t) ; ergo-macs
-;; (use-package xah-fly-keys :ensure t) ; xah fly keys (only for genuises with 9k iq)
-
 ;;
 ;; version specific settings (adjustments to font and window size)
 ;;
@@ -156,3 +228,5 @@
     ;;(set-frame-font "Iosevka-15" nil t)
     )))
 
+(provide 'init)
+;;; init.el ends here
